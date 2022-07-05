83b25e68bd383a737cab90311b6750d1431299fb
ktomaszewska97 <k.tomaszewska.97@gmail.com>
2022-07-05 16:06:55 +0200

    DB ready

--- core_xsjs/server.js
+++ core_xsjs/server.js
@@ -1,31 +1,86 @@
-/*eslint no-console: 0, no-unused-vars: 0*/
+/*eslint no-console: 0, no-unused-vars: 0, no-undef:0, no-process-exit:0*/
+/*eslint-env node, es6 */
+
 "use strict";
+const https = require("https");
+const port = process.env.PORT || 3100;
+const server = require("http").createServer();
+const express = require("express");
+const axios = require("axios");
 
-var xsjs  = require("@sap/xsjs");
-var xsenv = require("@sap/xsenv");
-var port  = process.env.PORT || 3000;
+//Initialize Express App for XSA UAA and HDBEXT Middleware
+const xsenv = require("@sap/xsenv");
+const passport = require("passport");
+const xssec = require("@sap/xssec");
+const xsHDBConn = require("@sap/hdbext");
+xsenv.loadEnv();
 
-var options = {
-	anonymous : true, // remove to authenticate calls
-	auditLog : { logToConsole: true }, // change to auditlog service for productive scenarios
-	redirectUrl : "/index.xsjs"
-};
+https.globalAgent.options.ca = xsenv.loadCertificates();
+global.__base = __dirname + "/";
+global.__uaa = process.env.UAA_SERVICE_NAME;
 
-// configure HANA
-try {
-	options = Object.assign(options, xsenv.getServices({ hana: {tag: "hana"} }));
-} catch (err) {
-	console.log("[WARN]", err.message);
-}
+//logging
+let logging = require("@sap/logging");
+let appContext = logging.createAppContext();
 
-// configure UAA
-try {
-	options = Object.assign(options, xsenv.getServices({ uaa: {tag: "xsuaa"} }));
-} catch (err) {
-	console.log("[WARN]", err.message);
-}
+//Initialize Express App for XS UAA and HDBEXT Middleware
+let app = express();
 
-// start server
-xsjs(options).listen(port);
+//Build a JWT Strategy from the bound UAA resource
+passport.use("JWT", new xssec.JWTStrategy(xsenv.getServices({
+	uaa: {
+		tag: "xsuaa"
+	}
+}).uaa));
 
-console.log("Server listening on port %d", port);
+//Add XS Logging to Express
+app.use(logging.middleware({
+	appContext: appContext,
+	logNetwork: true
+}));
+
+//Add Passport JWT processing
+app.use(passport.initialize());
+
+let hanaOptions = xsenv.getServices({
+	hana: {
+		plan: "hdi-shared"
+	}
+});
+
+hanaOptions.hana.pooling = true;
+//Add Passport for Authentication via JWT + HANA DB connection as Middleware in Expess
+app.use(
+	xsHDBConn.middleware(hanaOptions.hana),
+	passport.authenticate("JWT", {
+		session: false
+	})
+);
+
+//Setup Additional Node.js Routes
+require("./router")(app, server);
+
+//Start the Server
+server.on("request", app);
+server.listen(port, function () {
+	console.info(`HTTP Server: ${server.address().port}`);
+});
+
+axios
+    .get(
+        'https://api.litmoseu.com/v1.svc/courses?', {
+            params: {
+                source: 'simpleapp',
+                format: 'json',
+                limit: 1000
+            },
+            headers: {
+                'apikey': '5bfcf18f-34ff-45d8-8e49-86bc71acfd91'
+            }
+        })
+    .then((response) => {
+        console.log(response.data);
+});
+
+
+
